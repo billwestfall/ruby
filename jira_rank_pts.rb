31ca0d094c85aee178c8b8a6cae5343fd99c185b
billwestfall <billwestfall@yahoo.com>
2015-08-30 14:37:18 -0700

    Update jira_rank_pts.rb
    Break the hours effected into relative rank

--- jira_rank_pts.rb
+++ jira_rank_pts.rb
@@ -1,20 +1,38 @@
-# run as curl -X PUT --header "Content-Type: application/json" --data @pts.txt https://user:'password'@jira.test.com/jira/rest/api/2/issue/ISSUE-9076
-
 require 'rubygems'
 require 'highline/import'
 require 'mathn'
 
 def jira_rank
 
-  first, second, third = ARGV
+  first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth = ARGV
   # Users effected (1,25,2500,250k)
-  @x = first.to_f
+  @x = first.to_i
+  # First user relative benefit (1-10)
+  @c = second.to_i
+  # Second user relative benefit (1-10)
+  @d = third.to_i
+  # First user relative cost (1-10)
+  @e = fourth.to_i
+  # Second user relative cost (1-10)
+  @f = fifth.to_i
+  # First user relative risk (1-10)
+  @g = sixth.to_i
+  # Second user relative risk (1-10)
+  @h = seventh.to_i
   # Hours out of the release month effected (1-720)
-  @y = second.to_f
-  # Hours of CMO work needed (1-40)
-  @z = third.to_f
+  # @y = second.to_f
+  # First user hours of work needed (1-40)
+  @z = eighth.to_f
+  # Second user hours of work needed (1-40)
+  @b = ninth.to_f
+  # Urgency rank on ticket, 1 for Must Fix and 2 for Blocking
+  @a = tenth.to_i
 
-  g = (Math.log(Math.log(@x))*(@y)) + (10/@z)
+  h = ([@z, @b].max - [@z, @b].min) / (3.3)
+  i = (@z + @b)/2
+  j = h + i
+  @y = 4*((@c + @d)/2) - ((@e + @f)/2) - ((@g + @h)/2)
+  g = (Math.log(Math.log(@x))*(@y)) + (10/j) + (@a)
 
   puts %<{"fields": {"customfield_10572": #{g.round(4)}}}>
 
