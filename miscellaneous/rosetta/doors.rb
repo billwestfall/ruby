026327b45a564648bd2f3189885a3ca58e298c00
Bill Westfall <billwestfall@yahoo.com>
2017-05-20 11:04:44 -0700

    Update doors.rb

--- miscellaneous/rosetta/doors.rb
+++ miscellaneous/rosetta/doors.rb
@@ -1,1 +1,11 @@
 # trying a different take on the 100 doors problem
+
+$doors = 100
+$prize = Random.rand(100)
+
+while $doors > 0
+  if $doors = $prize
+  puts ("The prize is behind door " $prize)
+  else
+  $doors = $doors -=1
+end
