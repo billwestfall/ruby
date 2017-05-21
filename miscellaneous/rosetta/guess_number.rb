62212bdf0003e81c4ba11a4b98907dbe52f37715
Bill Westfall <billwestfall@yahoo.com>
2017-05-21 12:25:46 -0700

    Update guess_number.rb

--- miscellaneous/rosetta/guess_number.rb
+++ miscellaneous/rosetta/guess_number.rb
@@ -6,9 +6,9 @@ puts $computer
 puts "Please enter a number between 1 and 10"
 $guess = gets.chomp
 puts $guess
-comp_no = $computer.to_i
-guess_no = $guess.to_i
-  if $comp_no = $guess_no
+$comp_no = $computer.to_i
+$guess_no = $guess.to_i
+  if $computer = $guess
   puts ("You guessed the right number #$comp_no")
   else
   puts ("Oops the number is actually #$comp_no")
