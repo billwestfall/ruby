cea2c28bb80f15a1bb3ba4a6a8a49bb696e6ecc6
Bill Westfall <billwestfall@yahoo.com>
2017-05-20 18:30:18 -0700

    Update babbage.rb

--- miscellaneous/rosetta/babbage.rb
+++ miscellaneous/rosetta/babbage.rb
@@ -1,14 +1,14 @@
 
 # trying to solve the Babbage problem in Ruby
 
-$doors = 100
-$prize = Random.rand(100)
+$i = 1
 
-while $doors > 0
-  if $doors = $prize
-  puts ("The prize is behind door #$prize")
+while $i > 0
+  $babbage = $i * $i
+  if $babbage.include? "269696"
+  puts ("The Babbage number is #$babbage")
     break
   else
-  $doors = $doors -=1
+  $i = $i +=1
   end
 end
