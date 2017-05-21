8ea88feed9c2080083281897ea1760dfb3d768e0
Bill Westfall <billwestfall@yahoo.com>
2017-05-20 18:39:35 -0700

    Update babbage.rb

--- miscellaneous/rosetta/babbage.rb
+++ miscellaneous/rosetta/babbage.rb
@@ -6,7 +6,7 @@ $i = 1
 while $i > 0
   $babbage = $i * $i
   if $babbage.to_s.include? "269696"
-  puts ("The Babbage number is #$babbage")
+  puts ("The Babbage number is #$i and the root is #$babbage")
     break
   else
   $i = $i +=1
