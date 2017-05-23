f30bac34e5f95ec12c6b791c434c77c7ed713769
Bill Westfall <billwestfall@yahoo.com>
2017-05-23 16:37:43 -0700

    Update random_32.rb

--- miscellaneous/rosetta/random_32.rb
+++ miscellaneous/rosetta/random_32.rb
@@ -1,3 +1,10 @@
+# run as
+# MacBook-Air:rosetta billw$ ruby random_32.rb 
+# 1107922487
+# MacBook-Air:rosetta billw$ ruby random_32.rb 
+# 1345320271
+# MacBook-Air:rosetta billw$ ruby random_32.rb 
+# 1080584813
 
 $X = rand(0..2147483647)
 puts($X)
