5df00c5d1029876cd81dfc97feb4e6b881c28fd6
Bill Westfall <billwestfall@yahoo.com>
2017-05-24 21:44:03 -0700

    Create collatz.rb

---
+++ miscellaneous/rosetta/collatz.rb
@@ -0,0 +1,13 @@
+# generate collatz number 
+
+puts "Please enter an integer"
+$my_num = gets.chomp
+$my_no = $my_num.to_i
+while $my_num != 1
+  if $my_no.even
+  $my_no = $my_no / 2
+  puts $my_no
+  else
+  $my_no = (3 * $my_no) + 1
+  end
+end
