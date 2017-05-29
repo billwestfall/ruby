9c7c9deaa8e3fec397355452ec0e6b10f4e9edb0
Bill Westfall <billwestfall@yahoo.com>
2017-05-28 19:48:39 -0700

    Create windowed_app.rb

---
+++ miscellaneous/rosetta/windowed_app.rb
@@ -0,0 +1,22 @@
+require 'tk'
+
+def myproc
+   puts "The user says OK."
+   exit
+end
+
+root = TkRoot.new
+btn_OK = TkButton.new(root) do
+   text "OK"
+   borderwidth 5
+   underline 0
+   state "normal"
+   cursor "watch"
+   font TkFont.new('times 20 bold')
+   foreground  "red"
+   activebackground "blue"
+   relief      "groove"
+   command (proc {myproc})
+   pack("side" => "right",  "padx"=> "50", "pady"=> "10")
+end
+Tk.mainloop
