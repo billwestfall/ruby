0b8fd30d0ec702f562a18179f2f41011c8e09358
Bill Westfall <billwestfall@yahoo.com>
2017-06-17 11:13:18 -0700

    Update boolean.rb

--- miscellaneous/rosetta/boolean.rb
+++ miscellaneous/rosetta/boolean.rb
@@ -1,3 +1,9 @@
 puts "Ruby uses && for and , || for or, for example: "
 puts "true && true = "  
 puts true && true
+puts "true && false = "
+true && false
+puts "true || true = "
+puts true || true
+puts "true || false = "
+puts true || false
