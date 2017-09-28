a407f160a77b368338d14d6be53cf89fa1698dc3
Bill Westfall <billwestfall@yahoo.com>
2017-09-28 16:01:22 -0700

    Update greeter_spec.rb

--- rspec/chapt02/greeter_spec.rb
+++ rspec/chapt02/greeter_spec.rb
@@ -8,6 +8,6 @@ describe "Rspec Greeter" do
   it "should say 'Hello RSpec!' when it receives the greet() message" do
     greeter = RSpecGreeter.new
     greeting = greeter.greet
-    greeting.should == "Hello Rspec!"
+    greeting.should == "Hello RSpec!"
   end
 end
