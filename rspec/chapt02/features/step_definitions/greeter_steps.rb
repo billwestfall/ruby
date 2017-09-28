1b26b2891df72487cbe31699739b335b6cafae9c
Bill Westfall <billwestfall@yahoo.com>
2017-09-28 16:11:57 -0700

    Create greeter_steps.rb

---
+++ rspec/chapt02/features/step_definitions/greeter_steps.rb
@@ -0,0 +1,11 @@
+Given(/^a greeter$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I send it the greet message$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
