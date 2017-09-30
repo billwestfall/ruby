156f1a22e1a3f228e09f80e2c37885c11ba2229e
Bill Westfall <billwestfall@yahoo.com>
2017-09-30 10:37:31 -0700

    Update steps.rb

--- rspec/chapt04/features/step_definitions/steps.rb
+++ rspec/chapt04/features/step_definitions/steps.rb
@@ -1,3 +1,17 @@
+class Output
+  def messages
+    @messages ||= []
+  end
+  
+  def puts(message)
+    messages << message
+  end
+end
+
+  def output
+    @output ||= Output.new
+  end
+
 Given(/^I am not yet playing$/) do
 
 end
