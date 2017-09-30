e08fdc396479eba86c8ac6c454262cc1f5fe3558
billw@concur.com <billw@concur.com>
2017-09-30 09:56:40 -0700

    add call to game

--- rspec/chapt04/features/step_definitions/steps.rb
+++ rspec/chapt04/features/step_definitions/steps.rb
@@ -1,13 +1,13 @@
 Given(/^I am not yet playing$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+
 end
 
 When(/^I start a new game$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  Codebreaker::Game.new.start
 end
 
 Then(/^I should see "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+
 end
 
 Given(/^the secret code is "([^"]*)"$/) do |arg1|
