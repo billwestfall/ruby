954d1c811fe630c5cfe5959aa536718f1da45210
Bill Westfall <billwestfall@yahoo.com>
2017-10-12 15:03:26 -0700

    Update tour03.rb

--- programming/chapt01/tour03.rb
+++ programming/chapt01/tour03.rb
@@ -2,9 +2,9 @@ puts(1..3)
 puts(1...3)
 
 generation = case birthyear
-  when 1946..1963: puts("Baby Boomer")
-  when 1964..1976: puts("Generation X")
-  when 1978..2000: puts("Generation Y")
+  when 1946..1963, puts("Baby Boomer")
+  when 1964..1976, puts("Generation X")
+  when 1978..2000, puts("Generation Y")
 else nil
 end
 
