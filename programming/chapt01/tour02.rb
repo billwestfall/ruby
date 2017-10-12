eb52b92c402f5aaab42eac8b88d8cf641c295e69
Bill Westfall <billwestfall@yahoo.com>
2017-10-12 14:42:46 -0700

    Update tour02.rb

--- programming/chapt01/tour02.rb
+++ programming/chapt01/tour02.rb
@@ -23,3 +23,31 @@ def Math.square(x)
 end
 
 puts(Math.square(6))
+
+x = 1
+puts(x)
+x += 1
+puts(x)
+y = 9
+y -= 1
+puts(y)
+
+x, y = 14, 27
+puts(x, y)
+
+x, y = y, x
+puts(x, y)
+
+u,v,w = [2,6,86]
+puts(u,v,w)
+
+def polar(x,y)
+  theta = Math.atan2(y,x)
+  r = Math.hypot(x,y)
+  [r, theta]
+end
+
+distance, angle = polar(2,2)
+
+puts(distance)
+puts(angle)
