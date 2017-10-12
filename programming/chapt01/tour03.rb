e6c7df0e63ae4e5a41cdb3bf3e512c16e927ced0
Bill Westfall <billwestfall@yahoo.com>
2017-10-12 15:08:19 -0700

    Update tour03.rb

--- programming/chapt01/tour03.rb
+++ programming/chapt01/tour03.rb
@@ -1,14 +1,14 @@
-puts(1..3)
-puts(1...3)
+#puts(1..3)
+#puts(1...3)
 
-generation = case birthyear
-  when 1946..1963, puts("Baby Boomer")
-  when 1964..1976, puts("Generation X")
-  when 1978..2000, puts("Generation Y")
-else nil
-end
+#generation = case birthyear
+#  when 1946..1963, puts("Baby Boomer")
+#  when 1964..1976, puts("Generation X")
+#  when 1978..2000, puts("Generation Y")
+#else nil
+#end
 
-generation(1964)
+#generation(1964)
 
 def are_you_sure?
   while true
