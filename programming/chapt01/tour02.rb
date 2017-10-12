5143f969598a984e86d6a24c3d08dd184d9f2db1
Bill Westfall <billwestfall@yahoo.com>
2017-10-12 14:29:36 -0700

    Update tour02.rb

--- programming/chapt01/tour02.rb
+++ programming/chapt01/tour02.rb
@@ -1,3 +1,12 @@
 x = 5
 y = 4
 minimum = if x < y then puts(x) else puts(y) end
+puts(1+2)
+puts(1*2)
+puts(1+2==3)
+puts(2**1024)
+puts("Ruby" + "rocks")
+puts("Ruby!"*3)
+puts("%d %s" % [3, "rubies"])
+max = x > y ? x : y
+puts(max)
