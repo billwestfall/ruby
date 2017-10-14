00f88154aa4b23daa3c1eb9e5eed68a8853a976e
Bill Westfall <billwestfall@yahoo.com>
2017-10-13 20:37:19 -0700

    Update tour05.rb

--- programming/chapt01/tour05.rb
+++ programming/chapt01/tour05.rb
@@ -1,1 +1,18 @@
 module Sudoku
+
+  class Puzzle
+    
+    ASCII = ".123456789"
+    BIN = "\000\001\002\003\004\005\006\007\010\011"
+    
+    def initialize(lines)
+      if (lines.respond_to? :join)
+        s = lines.join
+      else
+        s = lines.dup
+      end
+      
+      s.gsub!(/\s/, "")
+      raise Invalid, "Grid is the wrong size" unless s.size == 81
+      
+      
