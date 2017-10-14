02e92233c6e566624a7e845df412c71ae4affeff
Bill Westfall <billwestfall@yahoo.com>
2017-10-13 19:44:26 -0700

    Update tour04.rb

--- programming/chapt01/tour04.rb
+++ programming/chapt01/tour04.rb
@@ -1,1 +1,19 @@
 class Range
+  def by(step)
+    x self.begin
+    if exclude_end?
+      while x < self.end
+        yield x
+        x += step
+      end
+    else
+      while x <= self.end
+        yield x
+        x += step
+      end
+    end
+  end
+end
+
+(0..10).by(2) {|x| print x}
+(0...10).by(2) {|x| print x}
