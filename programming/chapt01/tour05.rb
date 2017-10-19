4d1ed5d1d786ea235265529617d74fcc5e04e947
Bill Westfall <billwestfall@yahoo.com>
2017-10-19 12:48:34 -0700

    Update tour05.rb

--- programming/chapt01/tour05.rb
+++ programming/chapt01/tour05.rb
@@ -15,4 +15,28 @@ module Sudoku
       s.gsub!(/\s/, "")
       raise Invalid, "Grid is the wrong size" unless s.size == 81
       
+      if i = s.index(/[^123456789\.]/)
+        raise Invalid, "Illegal character #{s[i,1]} in puzzle"
+      end
       
+      s.tr!(ASCII, BIN)
+      @grid = s.unpack('c*')
+      
+      raise Invalid, "Initial puzzle has duplicates" if has_duplicates?
+    end
+    
+    def to_s
+      (0..8).collect{|r| @grid[r*9,9].pack('c9')}.join("\n").tr(BIN,ASCII)
+    end
+    
+    def dup
+      copy = super
+      @grid = @grid.dup
+      copy
+    end
+    
+    def [](row, col)
+      @grid[row*9 + col]
+    end
+    
+    
