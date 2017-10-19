4a5f4de9d984b66f505ab05ad92bd4669b1e59b9
Bill Westfall <billwestfall@yahoo.com>
2017-10-19 14:18:07 -0700

    Update 001.rb

--- programming/chapt03/001.rb
+++ programming/chapt03/001.rb
@@ -1,2 +1,15 @@
 puts( [5/2, 5.0/2, 5/2.0] )
 puts( [5.0.div(2), 5.0.fdiv(2), 5.quo(2)] )
+
+x = 5%2
+puts(x)
+
+q,r = 10.divmod 3
+puts(q)
+puts(r)
+
+puts( x**4 )
+puts( x**-1 )
+puts( x**(1/3.0) )
+puts( x**(1/4) )
+puts( x**(1.0/4.0) )
