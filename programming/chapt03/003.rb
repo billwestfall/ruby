9d5a12aaafb6e81a953196b5cd86a43178afd990
Bill Westfall <billwestfall@yahoo.com>
2017-10-20 16:38:07 -0700

    Update 003.rb

--- programming/chapt03/003.rb
+++ programming/chapt03/003.rb
@@ -1,8 +1,11 @@
 i = 0
-while i < 5
-  my_char=[*"\u4E00".."\u9FFF"].sample(10)
-  b = my_char.shift.strip
-  puts(b)
-  sleep 2
+while i < 5000
+  my_char=[*"\u00A1".."\u9FFF"].sample(1)
+  my_charb=[*"\u4E00".."\u9FFF"].sample(1)
+  a = my_char.shift.strip
+  b = my_charb.shift.strip
+  print(a)
+  print(b)
+#  sleep 2
   i = i + 1
 end
