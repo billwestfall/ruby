be8769cb163eba2b280ac5d42e16d597850a0399
Bill Westfall <billwestfall@yahoo.com>
2017-10-23 14:10:03 -0700

    Update 005.rb

--- programming/chapt03/005.rb
+++ programming/chapt03/005.rb
@@ -1,4 +1,5 @@
 s="hello"
+puts(s)
 puts(s[0])
 puts(s[1])
 puts(s[s.length])
@@ -7,3 +8,8 @@ puts(s[-2,1])
 puts(s.length)
 puts(s[2..3])
 puts(s[-3..-1])
+
+while(s["l"])
+  s["l"] = "L";
+  puts(s)
+end
