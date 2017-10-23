2fd684601df1e3275e3fe0abe6a820091e17ae24
Bill Westfall <billwestfall@yahoo.com>
2017-10-23 14:20:16 -0700

    Update 003.rb

--- programming/chapt03/003.rb
+++ programming/chapt03/003.rb
@@ -2,6 +2,7 @@ i = 0
 while i < 5000
   my_char=[*"\u00A1".."\u9FFF"].sample(1)
   my_charb=[*"\u4E00".."\u9FFF"].sample(1)
+  my_charc=[*"\u3040".."\u30FF"].sample(1)
   a = my_char.shift.strip
   b = my_charb.shift.strip
   print(a)
