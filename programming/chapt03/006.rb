015b4d3d1a53a7694afc65411ad950b3b5f6b4ef
Bill Westfall <billwestfall@yahoo.com>
2017-10-24 16:16:18 -0700

    Update 006.rb

--- programming/chapt03/006.rb
+++ programming/chapt03/006.rb
@@ -4,4 +4,15 @@ puts([1, 2, 3])
 print([-10...0,0..10,])
 print([[1,2],[3,4],[5]])
 print([x+y, x-y, x*y])
-print([])
+puts([])
+words=%w[this is a test]
+puts(words)
+
+a=[0,1,4,9,16]
+puts(a[0])
+puts(a[-1])
+puts(a[-2])
+puts(a[a.size-1])
+puts(a[-a.size])
+puts(a[8])
+puts(a[-8])
