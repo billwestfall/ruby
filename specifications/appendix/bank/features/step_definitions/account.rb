531ec44deba2f491b697a00bd35b13ff31df1607
billw@concur.com <billw@concur.com>
2017-11-09 13:49:24 -0800

    Add working steos

--- specifications/appendix/bank/features/step_definitions/account.rb
+++ specifications/appendix/bank/features/step_definitions/account.rb
@@ -1,17 +1,22 @@
 require "rspec/expectations"
 
+Given(/^a client called Simona$/) do
+  @simona = Client.new(name: "Simona")
+end
+
 Given(/^a bank account in Simona's name$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  step "a client called Simona"
+  @simona.account = Account.new
 end
 
 Given(/^that her current account balance is \$(\d+)$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+  @simona.account.balance = arg1.to_i
 end
 
 When(/^she deposits \$(\d+)$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+  @simona.account.deposit(arg1.to_i)
 end
 
-Then(/^the account balance should be \$(\d+)$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^the account balance should be \$(\d+)$/) do |final_balance|
+  expect(@simona.account.balance).to eq final_balance.to_i
 end
