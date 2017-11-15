57fb25279142d8f29603583a176402feeaf8abba
billw@concur.com <billw@concur.com>
2017-11-15 15:43:12 -0800

    Add scenarios

--- specifications/002/001.feature
+++ specifications/002/001.feature
@@ -4,4 +4,13 @@ Feature:  Scheduling
   You can find more detailed scenarios in the rest of the files inside the *meetings* folder in the specifications suite.
 
   Scenario: Create a meeting
-  
+
+  Given Mike, a member fo our team
+  When Mike chooses 2 p.m. as a start time for his meeting
+  Then he should be able to save his meeting
+
+  Scenario: Cancelling a meeting
+
+  Given Mike has a meeting scheduled
+  When Mike chooses to cancel a meeting on his schedule
+  Then the meeting will be removed from his calendar
---
+++ specifications/002/features/step_definitions/steps.rb
@@ -0,0 +1,23 @@
+Given(/^Mike, a member fo our team$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^Mike chooses (\d+) p\.m\. as a start time for his meeting$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^he should be able to save his meeting$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^Mike has a meeting scheduled$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^Mike chooses to cancel a meeting on his schedule$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the meeting will be removed from his calendar$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
