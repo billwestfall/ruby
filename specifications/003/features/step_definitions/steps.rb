921a36be1878a0ad18d42c273528892e541f8a42
Bill Westfall <billwestfall@yahoo.com>
2017-12-08 15:46:58 -0800

    Update steps.rb

--- specifications/003/features/step_definitions/steps.rb
+++ specifications/003/features/step_definitions/steps.rb
@@ -1,27 +1,27 @@
 When(/^Vladimir writes a sentence that is too long:$/) do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  puts("a")
 end
 
 Then(/^he should be notified that the sentence could be shortened$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  puts("b")
 end
 
 Then(/^he should be notified that it could be shortened in the Edit Mode$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+    puts("c")
 end
 
 Then(/^he shouldn't be notified about that in the Write Mode$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+    puts("d")
 end
 
 Given(/^text with a sentence that is too long:$/) do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+    puts("e")
 end
 
 When(/^the content is analyzed$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+    puts("f")
 end
 
 Then(/^the sentence that is too long should be detected$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+    puts("g")
 end
