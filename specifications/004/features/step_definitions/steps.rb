3b9933f58bb505f85514f9db21b54f129e965b0f
Bill Westfall <billwestfall@yahoo.com>
2017-12-11 16:12:50 -0800

    Create steps.rb

---
+++ specifications/004/features/step_definitions/steps.rb
@@ -0,0 +1,19 @@
+Given(/^a PDF book in Simona's cart$/) do
+  puts("this")
+end
+
+When(/^she pays for it$/) do
+  puts("scenario")
+end
+
+Then(/^the book should be sent to a mobile device$/) do
+  puts("is")
+end
+
+Given(/^an audiobook in Simona's cart$/) do
+  puts("very")
+end
+
+Then(/^the book should be sent over email$/) do
+ puts("redundant")
+end
