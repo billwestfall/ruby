538f30516358b4a5e355bcbd7716b6c084c1dfa2
Bill Westfall <billwestfall@yahoo.com>
2017-12-11 16:15:12 -0800

    Update steps.rb

--- specifications/004/features/step_definitions/steps.rb
+++ specifications/004/features/step_definitions/steps.rb
@@ -17,3 +17,19 @@ end
 Then(/^the book should be sent over email$/) do
  puts("redundant")
 end
+
+Given(/^a hardcover book in Simona's cart$/) do
+  puts("and")
+end
+
+Then(/^the book should be shipped physically$/) do
+  puts("now")
+end
+
+Given(/^a paperback book in Simona's cart$/) do
+  puts("it's")
+end
+
+Given(/^an Audio CD book in Simona's cart$/) do
+  puts("worse")
+end
