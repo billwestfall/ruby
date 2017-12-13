4c293e9ea0068c78edb21f6378fc3ce7bf77f8dc
Bill Westfall <billwestfall@yahoo.com>
2017-12-13 14:16:52 -0800

    Update steps.rb

--- specifications/005/features/step_definitions/steps.rb
+++ specifications/005/features/step_definitions/steps.rb
@@ -49,3 +49,7 @@ end
 Then(/^he should only pay \$(\d+)\.(\d+) for the items in his cart$/) do |arg1, arg2|
   puts("Chopin")
 end
+
+Given(/^items like "([^"]*)" bundle costing \$(\d+)\.(\d+) in John's cart$/) do |arg1, arg2, arg3|
+  puts("Tschaikovsky")
+end
