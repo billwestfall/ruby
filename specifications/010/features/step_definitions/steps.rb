3b175263bb8e711162c76dae8b63f16f49ff9e42
Bill Westfall <billw@concur.com>
2018-01-04 12:20:43 -0800

    Add authentication feature

---
+++ specifications/010/features/step_definitions/steps.rb
@@ -0,0 +1,79 @@
+Given("two-factor auth for Simona") do
+  puts("a")
+end
+
+Given("Simona's desire to log in") do
+  puts("a")
+end
+
+When("she provides her username") do
+  puts("a")
+end
+
+When("she provides her password") do
+  puts("a")
+end
+
+When("she enters the correct code") do
+  puts("a")
+end
+
+Then("she should be logged in") do
+  puts("a")
+end
+
+When("she enters a wrong code") do
+  puts("a")
+end
+
+Then("she should be not logged in") do
+  puts("a")
+end
+
+Given("traditional auth for Simona") do
+  puts("a")
+end
+
+When("she enters no code") do
+  puts("a")
+end
+
+Given("Simona's previous logins were from the USA") do
+  puts("a")
+end
+
+When("somebody logs in to her account from the USA") do
+  puts("a")
+end
+
+Then("her account should be considered secure") do
+  puts("a")
+end
+
+Then("Simona should be notified about the security breach") do
+  puts("a")
+end
+
+When("somebody logs in to her account from the UK") do
+  puts("a")
+end
+
+Then("her account should be considered compromised") do
+  puts("a")
+end
+
+Given("Simona's previous logins were from the UK") do
+  puts("a")
+end
+
+Given("Simona was notified about the security breach") do
+  puts("a")
+end
+
+When("she confirms her identity via two-factor authentication") do
+  puts("a")
+end
+
+Then("she should be able to log herself out from all devices remotely") do
+  puts("a")
+end
