1774b650d1a8815946cfffb72689cb0057c344f3
Bill Westfall <billwestfall@yahoo.com>
2018-03-09 18:55:27 -0800

    Update 007.rb

--- programming/chapt03/007.rb
+++ programming/chapt03/007.rb
@@ -2,9 +2,9 @@ my_dir = %x[pwd]
 my_files = %x[ls]
 my_curl = %x[curl google.com]
 
-puts(my_dir)
-puts(my_files)
-puts(my_curl)
+# puts(my_dir)
+# puts(my_files)
+# puts(my_curl)
 
 if my_curl.include? "301"
    puts "PASS"
