258051da36cea8c7b48703276158b50441838c6f
Bill Westfall <billw@concur.com>
2018-03-09 18:05:30 -0800

    Basic test

---
+++ rspec/basic/game_spec.rb
@@ -0,0 +1,13 @@
+# game_spec.rb
+
+require './game'
+
+RSpec.describe Game do
+  describe "#score" do
+    it "returns 0 for an all gutter game" do
+      game = Game.new
+      20.times { game.roll(0) }
+      expect(game.score).to eq(0)
+    end
+  end
+end
