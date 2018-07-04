74af84785494040cbe9d3cc108a12505612fda15
Bill Westfall <billwestfall@yahoo.com>
2018-07-04 12:34:36 -0700

    hype machine test

---
+++ miscellaneous/webtest/scrape_hype.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'colorize'
+require 'highline/import'
+
+puts("Test Google via curl")
+puts("")
+sleep 3
+
+my_hype = %x[curl -X GET "https://hypem.com/popular" -H  "accept: application/json;charset=UTF-8" -H  "Content-Type: application/json"]
+
+puts("")
+puts("Testing Hype Machine")
+sleep 1
+puts("")
+
+if my_hype.include? "Hype Machine"
+   puts "Page PASS".green
+else
+  puts "Page FAIL".red
+end
