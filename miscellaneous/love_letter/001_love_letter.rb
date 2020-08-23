13a25446eef90657d9a53e4806731c4ea1fcc40b
Bill Westfall <billwestfall@yahoo.com>
2020-08-23 15:41:22 -0700

    Create 001_love_letter.rb

---
+++ miscellaneous/love_letter/001_love_letter.rb
@@ -0,0 +1,65 @@
+
+b = ["Guard1", "Guard2", "Guard3", "Guard4", "Guard5", "Priest1", "Priest2", "Baron1", "Baron2", "Handmaid1", "Handmaid2", "Prince1", "Prince2", "King", "Countess", "Princess"]
+c = b.sample
+puts [c]
+b.delete(c)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+d = b.sample
+puts [d]
+b.delete(d)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+e = b.sample
+puts [e]
+b.delete(e)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+f = b.sample
+puts [f]
+b.delete(f)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+g = b.sample
+puts [g]
+b.delete(g)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+h = b.sample
+puts [h]
+b.delete(h)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+i = b.sample
+puts [i]
+b.delete(i)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+j = b.sample
+puts [j]
+b.delete(j)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+k = b.sample
+puts [k]
+b.delete(k)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+l = b.sample
+puts [l]
+b.delete(l)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+m = b.sample
+puts [m]
+b.delete(m)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+n = b.sample
+puts [n]
+b.delete(n)
+puts "Ready for next card? "
+card_prompt = gets.chomp
+o = b.sample
+puts [o]
+b.delete(o)
