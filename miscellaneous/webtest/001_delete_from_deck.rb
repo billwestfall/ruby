569a9ae6e9320fc2a31067434f387cd67c83cecb
Bill Westfall <billwestfall@yahoo.com>
2020-08-23 12:15:42 -0700

    pick card and delete from deck

---
+++ miscellaneous/webtest/001_delete_from_deck.rb
@@ -0,0 +1,8 @@
+
+b = ["Ace Diamonds", "king Diamonds", "Queen Diamonds", "Jack Diamonds", "10 Diamonds", "9 Diamonds", "8 Diamonds", "7 Diamonds", "6 Diamonds", "5 Diamonds", "4 Diamonds", "3 Diamonds", "2 Diamonds", "Ace Spades", "king Spades", "Queen Spades", "Jack Spades", "10 Spades", "9 Spades", "8 Spades", "7 Spades", "6 Spades", "5 Spades", "4 Spades", "3 Spades", "2 Spades", "Ace Clubs", "king Clubs", "Queen Clubs", "Jack Clubs", "10 Clubs", "9 Clubs", "8 Clubs", "7 Clubs", "6 Clubs", "5 Clubs", "4 Clubs", "3 Clubs", "2 Clubs", "Ace Hearts", "king Hearts", "Queen Hearts", "Jack Hearts", "10 Hearts", "9 Hearts", "8 Hearts", "7 Hearts", "6 Hearts", "5 Hearts", "4 Hearts", "3 Hearts", "2 Hearts"]
+c = b.sample
+print [c]
+puts [""]
+b.delete(c)
+print b
+puts [" "]
