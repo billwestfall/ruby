a332777aea287835b02042f2991ff3058dac62f8
Bill Westfall <billwestfall@yahoo.com>
2020-09-06 12:06:31 -0700

    Update roul_test.rb

--- miscellaneous/webtest/roul_test.rb
+++ miscellaneous/webtest/roul_test.rb
@@ -1,10 +1,11 @@
 puts "What is your bet? "
 roul_prompt = gets.chomp
-b = [1..10]
+d = roul_prompt.to_i
+b = (1..10)
 c = b.sample
 puts [c]
   if b == c
     puts "you win"
   else
     puts "you lose"
-  end
+end
