0033f3d0661ffbded07fe341d4da78d55b294434
Bill Westfall <billwestfall@yahoo.com>
2020-09-06 12:47:15 -0700

    Update 001_roulette.rb

--- miscellaneous/roulette/001_roulette.rb
+++ miscellaneous/roulette/001_roulette.rb
@@ -1,18 +1,19 @@
 puts "What kind of bet do you want to make?: 1) straight bet"
 bet_type = gets.chomp
-puts "What is your bet? "
-roul_prompt = gets.chomp
 e = bet_type.to.i
 if e == 1
-d = roul_prompt.to.i
-b = rand(0..36)
-puts [b]
-  if b == d
-    puts "you win"
+  puts "What is your bet? "
+  roul_prompt = gets.chomp
+  d = roul_prompt.to.i
+  b = rand(0..36)
+  puts [b]
+    if b == d
+      puts "you win"
+    else
+      puts "you lose"
+    end
   else
-    puts "you lose"
-end
-else
-end
+  end
+
 
 
