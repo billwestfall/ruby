180d032d3dfe1f8df5f00d54d9b65cf3e4657ba4
Bill Westfall <billwestfall@yahoo.com>
2020-09-27 14:22:45 -0700

    Update lilypond.rb

--- miscellaneous/lilypond/lilypond.rb
+++ miscellaneous/lilypond/lilypond.rb
@@ -2,12 +2,11 @@
 
   $i = 0
     while $i < 80 do
-      print [ :o0, :o1, :o2, :o3, :o4, :o5, :o6, :o7, :o8, :o9, :<, :> ].sample
       print " "
-      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
-      print [ :-, :+, :"", :"" ].sample
-      print [ :"4", :"8", :"16", :"32", :"" ].sample
-#      print [ :/, :"" ].sample
+      print [ a, b, c, d, e, f, g ].sample
+      print [ is, es, "", "" ].sample
+      print [ "2", "4", "8", "16", "32", "", "", "" ].sample
+      print [ "'", ",", "", "", "" ].sample
       print " "
       $i +=1
     end
