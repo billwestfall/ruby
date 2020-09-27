37d51c53f783a694bca158b08db7323b6ce117eb
Bill Westfall <billwestfall@yahoo.com>
2020-09-27 14:24:58 -0700

    Update lilypond.rb

--- miscellaneous/lilypond/lilypond.rb
+++ miscellaneous/lilypond/lilypond.rb
@@ -3,8 +3,8 @@
   $i = 0
     while $i < 80 do
       print " "
-      print [ a, b, c, d, e, f, g ].sample
-      print [ is, es, "", "" ].sample
+      print [ "a", "b", "c", "d", "e", "f", "g" ].sample
+      print [ "is", "es", "", "" ].sample
       print [ "2", "4", "8", "16", "32", "", "", "" ].sample
       print [ "'", ",", "", "", "" ].sample
       print " "
