57c88867534289be9d76e53f5761c946dfded582
Bill Westfall <billwestfall@yahoo.com>
2021-05-16 15:46:01 -0700

    Update 001_you_guess.rb

--- miscellaneous/mastermind/001_you_guess.rb
+++ miscellaneous/mastermind/001_you_guess.rb
@@ -4,7 +4,7 @@ c = b.sample(1).to_s
 puts "What colors are your guess? "
 mm_prompt = gets.chomp
 mm_prompt_str = mm_prompt.to_s
-if mm_prompt == c
+if mm_prompt_str == c
   puts "You win! Colors were ", c
 else
   puts "You lose, color were ", c
