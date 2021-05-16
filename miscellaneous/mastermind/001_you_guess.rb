97fc6fbfa846322f16c6b383844a15709df60eb6
Bill Westfall <billwestfall@yahoo.com>
2021-05-16 16:36:38 -0700

    Update 001_you_guess.rb

--- miscellaneous/mastermind/001_you_guess.rb
+++ miscellaneous/mastermind/001_you_guess.rb
@@ -1,6 +1,6 @@
 
-b = ["W","B","U","R","G","Y"]
-c = b.sample(1).join()
+b = ["W","W","W","W","B","B","B","B","U","U","U","U","R","R","R","R","G","G","G","G","Y","Y","Y","Y"]
+c = b.sample(4).join()
 puts "What colors are your guess? "
 guess = gets.chomp
 if guess == c
