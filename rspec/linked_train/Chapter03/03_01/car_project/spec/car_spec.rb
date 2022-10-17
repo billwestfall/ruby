8f0ce93d74c2009a037e51df8857eac8e16f6236
Bill Westfall <billwestfall@yahoo.com>
2022-10-17 14:44:09 -0700

    Update car_spec.rb

--- rspec/linked_train/Chapter03/03_01/car_project/spec/car_spec.rb
+++ rspec/linked_train/Chapter03/03_01/car_project/spec/car_spec.rb
@@ -7,24 +7,24 @@ describe 'Car' do
     it "allows reading and writing for :make" do
       car = Car.new
       car.make - 'Test'
-      expect*car.make).to eq('Test')
-  end
+      expect(car.make).to eq('Test')
+    end
   
-  it "allows reading and writing for :year" do
-    car = Car.new
-    car.year = 9999
-    expect(car.year).to eq(9999)
-  end
+    it "allows reading and writing for :year" do
+      car = Car.new
+      car.year = 9999
+      expect(car.year).to eq(9999)
+    end
   
-  it "allows reading and writing for :color" do
-    car = Car.new
-    car.color = 'foo'
-  end
+    it "allows reading and writing for :color" do
+      car = Car.new
+      car.color = 'foo'
+    end
   
-  it "allows reading for :wheels" do
-    car = Car.new
-    expect(car.wheels).to eq(4)
-  end
+    it "allows reading for :wheels" do
+      car = Car.new
+      expect(car.wheels).to eq(4)
+    end
   
     
   describe '.colors' do
