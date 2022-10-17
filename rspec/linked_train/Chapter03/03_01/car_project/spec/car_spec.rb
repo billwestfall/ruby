4bc5f0f3c824467eaeb6524b2d0ccfb9fd32d7fc
Bill Westfall <billwestfall@yahoo.com>
2022-10-17 14:20:10 -0700

    Update car_spec.rb

--- rspec/linked_train/Chapter03/03_01/car_project/spec/car_spec.rb
+++ rspec/linked_train/Chapter03/03_01/car_project/spec/car_spec.rb
@@ -1,1 +1,9 @@
 require 'car'
+
+describe '.colors' do
+  
+  it "returns an array of color names" do
+    c = ['blue', 'black', 'red', 'green']
+    expect(Car.colors).to match_array(c)
+  end
+end
