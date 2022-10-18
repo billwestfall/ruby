6f2b07520c59f3120f2c1b8beef029360faba3e4
Bill Westfall <billwestfall@yahoo.com>
2022-10-18 15:09:50 -0700

    Create filtera.rb

---
+++ rspec/linked_train/Chapter04/04_10/car_project/spec/filtera.rb
@@ -0,0 +1,9 @@
+RSpec.describe "A spec file to demonstrate how RSpec Formatters work" do 
+   context 'when running some tests' do 
+      
+      it 'the test usually calls the expect() method at least once' do 
+         expect(1 + 1).to eq(1) 
+      end
+      
+   end 
+end
