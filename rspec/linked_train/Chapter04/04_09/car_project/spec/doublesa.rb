9328b9df9db829ef044592761e7789c4b0b5db78
Bill Westfall <billwestfall@yahoo.com>
2022-10-18 14:34:19 -0700

    Update doublesa.rb

--- rspec/linked_train/Chapter04/04_09/car_project/spec/doublesa.rb
+++ rspec/linked_train/Chapter04/04_09/car_project/spec/doublesa.rb
@@ -7,3 +7,26 @@ class ClassRoom
       @students.map(&:name).join(',') 
    end 
 end
+
+class ClassRoom 
+   def initialize(students) 
+      @students = students 
+   end
+   
+   def list_student_names 
+      @students.map(&:name).join(',') 
+   end 
+end
+
+describe ClassRoom do 
+   it 'the list_student_names method should work correctly' do 
+      student1 = double('student') 
+      student2 = double('student') 
+      
+      allow(student1).to receive(:name) { 'John Smith'} 
+      allow(student2).to receive(:name) { 'Jill Smith'} 
+      
+      cr = ClassRoom.new [student1,student2]
+      expect(cr.list_student_names).to eq('John Smith,Jill Smith') 
+   end 
+end
