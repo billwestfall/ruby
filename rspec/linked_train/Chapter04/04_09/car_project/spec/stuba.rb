7436a8da2468e4714f6d17537e9aa2d7b14e1e91
Bill Westfall <billwestfall@yahoo.com>
2022-10-18 14:39:23 -0700

    Update stuba.rb

--- rspec/linked_train/Chapter04/04_09/car_project/spec/stuba.rb
+++ rspec/linked_train/Chapter04/04_09/car_project/spec/stuba.rb
@@ -1,22 +1,23 @@
 class ClassRoom 
    def initialize(students) 
       @students = students 
-   End
+   end 
    
    def list_student_names 
       @students.map(&:name).join(',') 
    end 
+	
 end 
 
 describe ClassRoom do 
    it 'the list_student_names method should work correctly' do 
       student1 = double('student') 
-      student2 = double('student') 
+      student2 = double('student')
       
-      allow(student1).to receive(:name) { 'John Smith'}
-      allow(student2).to receive(:name) { 'Jill Smith'} 
+      student1.stub(:name).and_return('John Smith')
+      student2.stub(:name).and_return('Jill Smith') 
       
-      cr = ClassRoom.new [student1,student2]
+      cr = ClassRoom.new [student1,student2] 
       expect(cr.list_student_names).to eq('John Smith,Jill Smith') 
    end 
 end
