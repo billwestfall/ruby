5df369293e8a658b2dcac15f72a03066fa20af76
Bill Westfall <billwestfall@yahoo.com>
2022-10-20 14:03:29 -0700

    Update test001_spec.rb

--- rspec/capybara/001/spec/test001_spec.rb
+++ rspec/capybara/001/spec/test001_spec.rb
@@ -1,7 +1,7 @@
 feature "Signing in" do
 
   scenario "Signing in with correct credentials" do
-    visit 'https://www.google.com/'
+    visit '/'
     within("#session") do
       fill_in 'search', with: 'Success'
     end
