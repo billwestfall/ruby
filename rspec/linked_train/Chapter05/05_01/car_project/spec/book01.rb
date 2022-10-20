896e13553b7df7c539e4af6570a55c757afc95b8
Bill Westfall <billwestfall@yahoo.com>
2022-10-20 13:26:50 -0700

    Create book01.rb

---
+++ rspec/linked_train/Chapter05/05_01/car_project/spec/book01.rb
@@ -0,0 +1,12 @@
+def test_making_order
+  book = Book.new(:title => "RSpec Intro", :price => 20)
+  customer = Customer.new
+  order = Order.new(customer, book)
+
+  order.submit
+
+  assert(customer.orders.last == order)
+  assert(customer.ordered_books.last == book)
+  assert(order.complete?)
+  assert(!order.shipped?)
+end
