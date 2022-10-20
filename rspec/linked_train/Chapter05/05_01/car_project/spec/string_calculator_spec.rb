6c901f6e2209f51e3e27e48c2e08be6314e13c23
Bill Westfall <billwestfall@yahoo.com>
2022-10-20 13:35:51 -0700

    Update string_calculator_spec.rb

--- rspec/linked_train/Chapter05/05_01/car_project/spec/string_calculator_spec.rb
+++ rspec/linked_train/Chapter05/05_01/car_project/spec/string_calculator_spec.rb
@@ -1,5 +1,11 @@
 # spec/string_calculator_spec.rb
-require "string_calculator"
-
 describe StringCalculator do
+
+  describe ".add" do
+    context "given an empty string" do
+      it "returns zero" do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+  end
 end
