4100a0f6515289d6d99950354cb3d43d3f712d0e
Bill Westfall <billwestfall@yahoo.com>
2022-10-24 14:52:53 -0700

    Update rand01.rb

--- miscellaneous/intro/rand01.rb
+++ miscellaneous/intro/rand01.rb
@@ -2,7 +2,7 @@ require 'securerandom'
 require 'colorize'
 require 'colorized_string'
 
-text_colors = [black, light_black, red, light_red, green, light_green, yellow, light_yellow, blue, light_blue, magenta, light_magenta, cyan, light_cyan, white, light_white]
+text_colors = ["black", "light_black", "red", "light_red", "green", "light_green", "yellow", "light_yellow", "blue", "light_blue", "magenta", "light_magenta", "cyan", "light_cyan", "white", "light_white"]
 
 x = Random.rand(30)
 y = SecureRandom.hex(x)
