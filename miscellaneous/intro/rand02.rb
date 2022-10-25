d6ab82e7d5c30c9f8b86ad0ab84c72e9a089cf4b
Bill Westfall <billwestfall@yahoo.com>
2022-10-25 11:22:40 -0700

    Update rand02.rb

--- miscellaneous/intro/rand02.rb
+++ miscellaneous/intro/rand02.rb
@@ -5,7 +5,7 @@ require 'colorized_string'
 text_colors = ["black", "light_black", "red", "light_red", "green", "light_green", "yellow", "light_yellow", "blue", "light_blue", "magenta", "light_magenta", "cyan", "light_cyan", "white", "light_white"]
 text_mode = ["", ".bold", ".italic", ".underline", ".blink", ".swap"]
 
-x = Random.rand(1-3)
+x = Random.rand(1..3)
 y = SecureRandom.hex(x)
 z = text_colors.sample
 a = text_colors.sample
