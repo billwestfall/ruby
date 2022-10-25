bf58a22b87e4cd0d25291366ecdb261142708285
Bill Westfall <billwestfall@yahoo.com>
2022-10-25 10:14:02 -0700

    Update rand01.rb

--- miscellaneous/intro/rand01.rb
+++ miscellaneous/intro/rand01.rb
@@ -3,12 +3,14 @@ require 'colorize'
 require 'colorized_string'
 
 text_colors = ["black", "light_black", "red", "light_red", "green", "light_green", "yellow", "light_yellow", "blue", "light_blue", "magenta", "light_magenta", "cyan", "light_cyan", "white", "light_white"]
+team_mode = [".default", ".bold", ".italic", ".underline", ".blink", ".swap"]
 
-x = Random.rand(30)
+x = Random.rand(40)
 y = SecureRandom.hex(x)
 z = text_colors.sample
 a = text_colors.sample
+e = text_mode.sample
 
-b = "ColorizedString[\"" + y + "\"]." + z + ".on_" + a
+b = "ColorizedString[\"" + y + "\"]." + z + ".on_" + a + e
 d = b.to_s
 puts eval d
