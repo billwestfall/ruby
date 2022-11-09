2931e6e71712845b674b910a786066502430fc1b
Bill Westfall <billwestfall@yahoo.com>
2022-11-09 13:48:04 -0800

    Update boo01.rb

--- miscellaneous/intro/boo01.rb
+++ miscellaneous/intro/boo01.rb
@@ -10,3 +10,12 @@ puts x == 100 || x <= 50
 puts !x
 puts [1,2,3].include?(2)
 puts [1,2,3].include?(4)
+puts x.nil?
+puts 2.between?(1,5)
+puts [1,2,3].empty?
+puts [].empty?
+hash = {'a' => 1, 'b' => 2}
+puts hash
+puts hash.has_key?(:a)
+puts hash.has_value?(1)
+puts hash.has_value?(5)
