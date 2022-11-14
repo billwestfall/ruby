3c20ad60ecc9e1cfc948dfb80f1e7be862151faf
Bill Westfall <billwestfall@yahoo.com>
2022-11-14 14:43:25 -0800

    Update guess01.rb

--- miscellaneous/intro/guess01.rb
+++ miscellaneous/intro/guess01.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 x = rand(1..10)
+puts x
 
 puts "Guess a number from 1 to 10: "
 
@@ -8,7 +9,7 @@ num = gets
 y = num.to_i
 
   i = 1
-  while i < 3
+  while i <= 3
     if y == x
     puts "You got it!"
     exit
