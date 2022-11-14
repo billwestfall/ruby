5516ace0ee0628b08e9b828e0443cbc307ea7487
Bill Westfall <billwestfall@yahoo.com>
2022-11-14 12:55:43 -0800

    Update guess01.rb

--- miscellaneous/intro/guess01.rb
+++ miscellaneous/intro/guess01.rb
@@ -7,12 +7,12 @@ puts "Guess a number from 1 to 10: "
 num = gets
 y = num.to_i
 
-if y == x
-  puts "You got it!"
-  exit
-else
-   i = 1
+  i = 1
   while i < 3
+    if y == x
+    puts "You got it!"
+    exit
+  else
     puts "Enter a new guess: "
     num = gets
     y = num.to_i
