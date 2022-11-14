30e50461b4f100ab97bd89d3ca108529ce48f266
Bill Westfall <billwestfall@yahoo.com>
2022-11-14 15:11:43 -0800

    Update find01.rb

--- miscellaneous/intro/find01.rb
+++ miscellaneous/intro/find01.rb
@@ -11,3 +11,13 @@ puts "Is n divisible by 3?"
 (1..10).find do |n|
   puts n % 3 == 0
 end
+
+fruits = ['apple', 'banana', 'pear']
+
+puts fruits
+
+puts "Is name of fruit more than 5 characters?"
+
+fruits.find |fruit|
+  puts fruit.length > 5
+end
