5da81691ef21a7032f6f0d8d5f948a3b4874b094
Bill Westfall <billwestfall@yahoo.com>
2022-11-14 12:42:37 -0800

    Create guess01.rb

---
+++ miscellaneous/intro/guess01.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+x = rand[1..10]
+
+puts "Guess a number from 1 to 10: "
+
+num = gets
+y = num.to_i
+
+if y == x
+  puts "You got it!"
+  exit
+else
+   i = 1
+  while i < 4
+    puts "Enter a new guess: "
+    num = gets
+    y = num.to_i
+    i += 1
+  end
+end
