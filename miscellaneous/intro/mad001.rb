82cba6a663186fb83316f2c0a0d5d41afac7d99d
Bill Westfall <billwestfall@yahoo.com>
2022-11-15 13:37:14 -0800

    Create mad001.rb

---
+++ miscellaneous/intro/mad001.rb
@@ -0,0 +1,15 @@
+#!/env/usr/env ruby
+
+puts "Please enter a verb: "
+
+v = gets
+
+puts "Please enter an adjective: "
+
+a = gets
+
+puts "Please enter a noun: "
+
+n = gets
+
+puts "I #{v} the #{a} #{n}"
