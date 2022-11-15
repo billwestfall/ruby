2579e0e8607b4b0e94a4361775a78946e329f3ad
Bill Westfall <billwestfall@yahoo.com>
2022-11-15 15:21:42 -0800

    Update latin01.rb

--- miscellaneous/intro/latin01.rb
+++ miscellaneous/intro/latin01.rb
@@ -4,10 +4,11 @@ puts "Enter a word to covert to pig latin: "
 
 z = gets.chomp
 y = Array[z]
+x = y.first
 
 v = ['a', 'e', 'i', 'o', 'u']
 
-if y.intersection(v)
+if x.intersection(v)
   puts "Starts with a vowel!"
 else
   puts "Starts with a consanant"
