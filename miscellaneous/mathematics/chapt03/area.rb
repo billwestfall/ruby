153890c934adac945e2b735009fae6a7a7053f3e
Bill Westfall <billwestfall@yahoo.com>
2023-02-09 21:27:07 -0800

    Create area.rb

---
+++ miscellaneous/mathematics/chapt03/area.rb
@@ -0,0 +1,10 @@
+puts("Please enter the length: ")
+gets l
+puts("Please enter the  height: ")
+gets h
+puts("Please enter the width: ")
+gets w
+
+area = (2(l*h)) + (2(l*w)) + (2(w*h))
+puts("Total area is: ")
+puts area
