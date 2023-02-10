ec8305a5ece0b61c28fcfd489e0aa74396f80008
Bill Westfall <billwestfall@yahoo.com>
2023-02-09 21:33:23 -0800

    Update area.rb

--- miscellaneous/mathematics/chapt03/area.rb
+++ miscellaneous/mathematics/chapt03/area.rb
@@ -1,9 +1,9 @@
 puts("Please enter the length: ")
-l = gets
+l = gets.to_i
 puts("Please enter the  height: ")
-h = gets
+h = gets.to_i
 puts("Please enter the width: ")
-w = gets
+w = gets.to_i
 
 area = (2*l*h) + (2*l*w) + (2*w*h)
 puts("Total area is: ")
