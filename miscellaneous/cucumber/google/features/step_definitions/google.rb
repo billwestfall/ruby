08f175fdcafd075857734287e5bfead120494cd2
Bill Westfall <billwestfall@yahoo.com>
2024-03-25 12:43:04 -0700

    Update google.rb

--- miscellaneous/cucumber/google/features/step_definitions/google.rb
+++ miscellaneous/cucumber/google/features/step_definitions/google.rb
@@ -1,3 +1,6 @@
+require 'selenium-webdriver'
+require 'rspec'
+
 date = Time.now.strftime("%Y-%m-%d-%H-%M-%S")
 
 Given('I am on the Google homepage') do
@@ -9,4 +12,23 @@ end
 
 Then /^I should see "([^"]*)"$/ do |desc|
     expect(page).to have_content desc
+end
+
+Given("I am on the Google start page") do
+  @driver = Selenium::WebDriver.for :chrome
+  @driver.navigate.to "https://www.google.com"
+end
+
+Then("I should see the Google logo") do
+  logo = @driver.find_element(:id, "hplogo")
+  expect(logo.displayed?).to be(true)
+end
+
+Then("I should see the search input field") do
+  search_input = @driver.find_element(:name, "q")
+  expect(search_input.displayed?).to be(true)
+end
+
+After do
+  @driver.quit
 end
