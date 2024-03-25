b12aeb3805a79ff53e9c87415a59a158eb6d2d90
Bill Westfall <billwestfall@yahoo.com>
2024-03-25 10:28:37 -0700

    Update google.rb

--- miscellaneous/cucumber/google/features/step_definitions/google.rb
+++ miscellaneous/cucumber/google/features/step_definitions/google.rb
@@ -5,3 +5,7 @@ Given(/^I am on Google homepage$/) do
     @profile_id = ENV['MYSITE']
     visit "http://#{@profile_id}"
 end
+
+Then('I should see {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
