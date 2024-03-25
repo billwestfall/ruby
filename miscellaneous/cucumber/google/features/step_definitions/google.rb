6f96f84f2a8fc4617587fe401a06f126f3b68d20
Bill Westfall <billwestfall@yahoo.com>
2024-03-25 10:40:51 -0700

    Update google.rb

--- miscellaneous/cucumber/google/features/step_definitions/google.rb
+++ miscellaneous/cucumber/google/features/step_definitions/google.rb
@@ -3,7 +3,7 @@ date = Time.now.strftime("%Y-%m-%d-%H-%M-%S")
 Given /^I am on Google homepage$/ do
 #    visit 'https://www.google.com'
     @profile_id = ENV['MYSITE']
-    visit "http://#{@profile_id}"
+    visit "https://#{@profile_id}"
 end
 
 Then /^I should see "([^"]*)"$/ do |desc|
