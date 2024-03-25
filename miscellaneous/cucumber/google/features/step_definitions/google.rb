209a32f1298ba9d2cd0d155f7444aefb63f8fe98
Bill Westfall <billwestfall@yahoo.com>
2024-03-25 10:43:34 -0700

    Update google.rb

--- miscellaneous/cucumber/google/features/step_definitions/google.rb
+++ miscellaneous/cucumber/google/features/step_definitions/google.rb
@@ -2,7 +2,8 @@ date = Time.now.strftime("%Y-%m-%d-%H-%M-%S")
 
 Given('I am on the Google homepage') do
 #    visit 'https://www.google.com'
-    @profile_id = ENV['MYSITE']
+#    @profile_id = ENV['MYSITE']
+    @profile_id = "www.google.com"
     visit "https://#{@profile_id}"
 end
 
