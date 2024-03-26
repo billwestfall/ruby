313a2ffa530314329d65d7d26b659a1a6b529a5d
Bill Westfall <billwestfall@yahoo.com>
2024-03-26 16:01:35 -0700

    Update google.rb

--- miscellaneous/cucumber/google/features/step_definitions/google.rb
+++ miscellaneous/cucumber/google/features/step_definitions/google.rb
@@ -15,7 +15,7 @@ When /^I enter "([^"]*)" in the Search window$/ do |search|
 end
 
 When('I click the {string} button') do |string|
-  click string
+  click_on string
 end
 
 Then /^I should see "([^"]*)"$/ do |desc|
