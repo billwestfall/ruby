1228f1f224d02eb7b8619c50fe4835d0aa871008
Bill Westfall <billwestfall@yahoo.com>
2024-03-27 08:09:55 -0700

    Create browser.rb

---
+++ miscellaneous/cucumber/browser/step_definitions/browser.rb
@@ -0,0 +1,23 @@
+require 'selenium-webdriver'
+require 'rspec'
+
+date = Time.now.strftime("%Y-%m-%d-%H-%M-%S")
+
+Given('I am on the Google homepage') do
+#    visit 'https://www.google.com'
+#    @profile_id = ENV['MYSITE']
+    @profile_id = "www.google.com"
+    visit "https://#{@profile_id}"
+end
+
+When /^I enter "([^"]*)" in the Search window$/ do |search|
+  fill_in 'APjFqb', :with => search
+end
+
+When('I click the {string} button') do |string|
+  click_button(string)
+end
+
+Then /^I should see "([^"]*)"$/ do |desc|
+    expect(page).to have_content desc
+end
