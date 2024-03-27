c0a4ed9438503edb07eeddd0d697a3620b5d20de
Bill Westfall <billwestfall@yahoo.com>
2024-03-27 08:20:49 -0700

    Delete miscellaneous/cucumber/browser/support directory

--- miscellaneous/cucumber/browser/support/settings.rb
+++
@@ -1,7 +0,0 @@
-require 'capybara/cucumber'
-require 'capybara/poltergeist'
-require 'minitest/autorun'
-require 'headless'
-
-Capybara.default_driver = :selenium
-Capybara.javascript_driver = :headless
