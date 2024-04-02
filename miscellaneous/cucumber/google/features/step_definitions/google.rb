f88a74b813e861f28c9f96e84ee25ad2b8a84400
Bill Westfall <billwestfall@yahoo.com>
2024-04-02 15:07:24 -0700

    Update google.rb

--- miscellaneous/cucumber/google/features/step_definitions/google.rb
+++ miscellaneous/cucumber/google/features/step_definitions/google.rb
@@ -21,3 +21,22 @@ end
 Then /^I should see "([^"]*)"$/ do |desc|
     expect(page).to have_content desc
 end
+
+Given("I am on the Google home page") do
+  visit "https://www.google.com"
+end
+
+When("I enter {string} into the search field") do |search_term|
+  fill_in "q", with: search_term # Google's search input name is "q"
+end
+
+When("I click the {string} button") do |button_name|
+  # Google's search button can be tricky to interact with due to dynamic loading and JavaScript,
+  # so we'll use the 'name' attribute to find the button.
+  find_button(name: "btnK").click
+end
+
+Then("I should see results related to {string}") do |expected_result|
+  # Wait for the page to load and check that it includes the expected text.
+  expect(page).to have_content(expected_result)
+end
