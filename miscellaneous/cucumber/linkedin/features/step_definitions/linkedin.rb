9367393eb626b94036acf6fe83d60369c303f665
Bill Westfall <billwestfall@yahoo.com>
2024-10-14 11:26:55 -0700

    Update linkedin.rb

--- miscellaneous/cucumber/linkedin/features/step_definitions/linkedin.rb
+++ miscellaneous/cucumber/linkedin/features/step_definitions/linkedin.rb
@@ -1,7 +1,7 @@
 require 'selenium-webdriver'
 require 'rspec'
 require 'watir'
-require 'watir-webdriver'
+#require 'watir-webdriver'
 
 date = Time.now.strftime("%Y-%m-%d-%H-%M-%S")
 browser = Watir::Browser.new :chrome
